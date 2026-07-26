65e03c346ca50f99ab1693de19369d778e4ea8f1
Yash Gautam <yash.ishu.barehi@gmail.com>
2026-07-26 22:12:36 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+       unordered_map<int,int>mp;
+
+       for(auto num : nums){
+        mp[num]++;
+       }
+
+       for(auto it : mp){
+        if(it.second >= 2){
+            return true;
+        }
+       }
+       return false;
+    }
+};
